78e97557bb7247b03882111362627758e1b3dc82
paulportillo <yummta@gmail.com>
2021-10-08 19:11:28 -0500

    Refactor show images

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
 import useSWRInfinite from "swr/infinite";
+import clsx from "clsx";
+import { useEffect, useState } from "react";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
+const joinImagesData = (imagesData = []) => {
+  console.log(imagesData);
+  return imagesData.reduce((acc, cur) => {
+    return [...acc, ...cur.records];
+  }, []);
+};
+
 export default function Home() {
+  const [gallery, setGallery] = useState([]);
   const {
     data: imagesData,
     size,
@@ -14,8 +24,15 @@ export default function Home() {
       `https://api.harvardartmuseums.org/image?apikey=${
         process.env.NEXT_PUBLIC_APIKEY
       }&page=${pageIndex + 1}`,
-    fetcher
+    fetcher,
+    {
+      revalidateOnFocus: false,
+    }
   );
+
+  useEffect(() => {
+    setGallery(joinImagesData(imagesData));
+  }, [imagesData]);
 
   return (
     <div>
@@ -29,23 +46,24 @@ export default function Home() {
       </Head>
       <main>
         <div className="text-center max-w-7xl m-auto">
-          {imagesData &&
-            imagesData.map((imageData) => {
-              return imageData.records.map((image) => (
-                <div
-                  className="relative h-full overflow-hidden"
-                  key={image.imageid}
-                >
-                  <Image
-                    src={`${image.baseimageurl}?width=300`}
-                    alt={image.alttext}
-                    width={300}
-                    height={480}
-                    loading="lazy"
-                  />
-                </div>
-              ));
-            })}
+          {gallery.length &&
+            gallery.map((image, imageIndex) => (
+              <div
+                className={clsx(
+                  "relative h-full overflow-hidden",
+                  gallery.length - 3 === imageIndex && "border border-red-400"
+                )}
+                key={image.imageid}
+              >
+                <Image
+                  src={`${image.baseimageurl}?width=300`}
+                  alt={image.alttext}
+                  width={300}
+                  height={480}
+                  loading="lazy"
+                />
+              </div>
+            ))}
         </div>
         <button onClick={() => setSize(size + 1)}>Load More</button>
       </main>
